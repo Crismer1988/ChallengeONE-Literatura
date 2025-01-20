084de11c2fc0abd2f96f4c627b1736d0a993f953
Crismer1988 <crismer1988@gmail.com>
2025-01-19 21:50:44 -0500

    Primer_commit

---
+++ src/test/java/com/alura/desafios/Desafio/Literatura/DesafioLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.desafios.Desafio.Literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
